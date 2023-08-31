df4162503dc7fa2bd1e9cd70562aaf269129a6a7
Tinica Adrian <118120924+adriantinica@users.noreply.github.com>
2023-08-31 16:08:53 +0300

    Update ProductRepository.java

--- e-sop-DB/e-shop/src/main/java/student/example/orm/repositories/ProductRepository.java
+++ e-sop-DB/e-shop/src/main/java/student/example/orm/repositories/ProductRepository.java
@@ -16,7 +16,7 @@ public class ProductRepository extends Repository {
 
     // Private constructor to prevent direct instantiation from outside the class.
     private ProductRepository() {
-        super();
+        
     }
 
     
@@ -30,98 +30,102 @@ public class ProductRepository extends Repository {
     
 
 
-        public void create (Product product){
-            Statement st;
-            try {
-                st = conn.createStatement();
-                st.executeUpdate("INSERT INTO products VALUES ("
-                + product.getId()+ ", '"
-                + product.getName()+ "','"
-                + product.getImage()+"'"+
-                ") ");
+    public void create (Product product){
+        Statement st;
+        try {
+            st = conn.createStatement();
+            st.executeUpdate("INSERT INTO products VALUES ("
+             + product.getId()+ ", '"
+            + product.getName()+ "','"
+            + product.getImage()+"'"+
+            ") ");
+        } catch (SQLException e) {
+             
+            e.printStackTrace();
+        }
+    }
+
+    public Product readById(int id){
+        Statement st;
+        Product product= null;
+        try {
+            st = conn.createStatement();
+            ResultSet rs= st.executeQuery("SELECT *FROM products where id="+ id);
+            rs.next();
+            product = new Product(
+                rs.getInt(1),
+                rs.getString(2),
+                rs.getString(3));
             } catch (SQLException e) {
-                
-                e.printStackTrace();
-            }
-        }
-
-        public Product readById(int id){
-         Statement st;
-         Product product= null;
-            try {
-                st = conn.createStatement();
-                ResultSet rs= st.executeQuery("SELECT *FROM products where id="+ id);
-                rs.next();
-                product = new Product(
-                    rs.getInt(1),
-                    rs.getString(2),
-                    rs.getString(3));
-                } catch (SQLException e) {
                
-                e.printStackTrace();
-            }
-            return product;
+            e.printStackTrace();
         }
+            return product;
+    }
 
-        public List<Product> readAll(){
-            List<Product> products = new ArrayList<>();
-            Statement st = null;
-            ResultSet rs = null;
+    public List<Product> readAll(){
+        List<Product> products = new ArrayList<>();
+        Statement st = null;
+        ResultSet rs = null;
 
         
-                try {
-                   st = conn.createStatement();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-                try {
-                    rs = st.executeQuery("SELECT * FROM products");
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
+            try {
+                st = conn.createStatement();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            try {
+                rs = st.executeQuery("SELECT * FROM products");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
 
-               try {
-                   while (rs.next()) {
-                        int id = rs.getInt("id");
-                        String name = rs.getString("name");
-                        String image = rs.getString("image");
+            try {
+                while (rs.next()) {
+                    int id = rs.getInt("id");
+                    String name = rs.getString("name");
+                    String image = rs.getString("image");
 
-                        Product product = new Product(id, name, image);
-                     products.add(product);
+                    Product product = new Product(id, name, image);
+                    products.add(product);
                     }
-                } catch (SQLException e) {
-                
-                    e.printStackTrace();
-                }
-            return products;
-        }
-        // UPDATE
-        public void update(String name, String image, int id){
-           
-            Statement st;
-            try {
-                st = conn.createStatement();
-                st.executeUpdate("UPDATE products SET name ='"+ name+ "', image ='"+ image + "' WHERE id = " +id);
             } catch (SQLException e) {
                 
                 e.printStackTrace();
             }
+        return products;
+    }
+        
+    
+    // UPDATE
+    public void update(String name, String image, int id){
+          
+        Statement st;
+        try {
+            st = conn.createStatement();
+            st.executeUpdate("UPDATE products SET name ='"+ name+ "', image ='"+ image + "' WHERE id = " +id);
+        } catch (SQLException e) {
+            
+            e.printStackTrace();
         }
+    }
 
     
     
-        //DELETE
+    //DELETE
 
-        public void deleteById(int id){
-            Statement st;
-            try {
-                st = conn.createStatement();
-                st.executeUpdate("DELETE FROM products WHERE id ="+ id);
-            } catch (SQLException e) {
-                
-                e.printStackTrace();
-            }
+    public void deleteById(int id){
+        Statement st;
+        try {
+            st = conn.createStatement();
+            st.executeUpdate("DELETE FROM products WHERE id ="+ id);
+        } catch (SQLException e) {
+            
+            e.printStackTrace();
         }
+    }
+}
+
    
 
 
