b5d8a1bd46b1eb151f360f7ad98f5f979a12736a
Tinica Adrian <118120924+adriantinica@users.noreply.github.com>
2023-08-31 16:09:54 +0300

    Update App.java

--- e-sop-DB/e-shop/src/main/java/student/example/App.java
+++ e-sop-DB/e-shop/src/main/java/student/example/App.java
@@ -7,16 +7,16 @@ import student.example.orm.repositories.ProductRepository;
  * Hello world!
  *
  */
-public class App 
-{
+public class App {
     public static void main( String[] args ) throws SQLException{
 
 
     
-      ProductRepository pr = new ProductRepository.getInstance(); 
+      ProductRepository pr = ProductRepository.getInstance(); 
       pr.create(new Product(2,"Mango","2.jpg")); 
-      pr.create(new Product(3, "Tomato", "3.jpg"));     
-      
+      pr.create(new Product(3, "Tomato", "3.jpg"));
+      ProductRepository pg = ProductRepository.getInstance(); 
+      pg.create(new Product(5,"Banana","8.jpg"));
 
       Product product = pr.readById(1);
       System.out.println(product);
@@ -26,11 +26,10 @@ public class App
       System.out.println(pr.readAll());
       pr.update("Avocado","4.jpg",3);
       System.out.println(pr.readAll());
-
-
-      
-       
+      pr.deleteById(3);
+      System.out.println(pr.readAll());
     }
 
     
 }
+
